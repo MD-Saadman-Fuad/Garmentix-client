a7b26c9e58ade101f9613ec1d369d0fa0deb0e4f
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-12-06 15:45:09 +0600

    Env setup and Firebase Credentials hiding

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTMo5kJIwUTM7FTc3tcvpy00g1dxrVLqI",
-  authDomain: "garmentix-cecdb.firebaseapp.com",
-  projectId: "garmentix-cecdb",
-  storageBucket: "garmentix-cecdb.firebasestorage.app",
-  messagingSenderId: "963712142714",
-  appId: "1:963712142714:web:7be3f84bac1c39dcf61ac3"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
