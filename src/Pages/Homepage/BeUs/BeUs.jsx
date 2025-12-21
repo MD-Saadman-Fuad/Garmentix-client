c63af95045b23559ed61adda73dc340b37b6598b
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-12-22 03:50:31 +0600

    added redirect for netlify

--- src/Pages/Homepage/BeUs/BeUs.jsx
+++ src/Pages/Homepage/BeUs/BeUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../../../assets/agent-pending.png'
+import { Link } from 'react-router';
 const BeUs = () => {
     return (
         <div className='bg-primary flex flex-col md:flex-row justify-around items-center p-10 md:p-16 mt-20 rounded-2xl shadow-2xl'>
@@ -8,8 +9,8 @@ const BeUs = () => {
                 <h2 className='text-3xl md:text-4xl font-bold pt-6 md:pt-10 leading-tight'>Streamline Your Garment Production with Industry-Leading Technology</h2>
                 <p className='text-base md:text-lg mt-4 mb-8 md:mb-10 leading-relaxed opacity-95'>Join 500+ factories already transforming their production management. Track every stage from cutting to finishing, reduce delays by 40%, and achieve 99% on-time delivery with real-time analytics and seamless buyer collaboration.</p>
                 <div className='m-1 gap-4 flex flex-col sm:flex-row'>
-                    <button className="btn btn-primary rounded-full text-black font-semibold hover:scale-105 transition-transform">Start Free Trial</button>
-                    <button className="btn bg-transparent border-2 border-white text-white rounded-full font-semibold hover:bg-white hover:text-blue-700 transition-all">Schedule a Demo</button>
+                    <Link to={"/products"} className="btn btn-primary border-white rounded-full text-white font-semibold hover:scale-105 transition-transform">Start Your Buying Journey</Link>
+                    <Link to={"/register"} className="btn bg-transparent border-2 border-white text-white rounded-full font-semibold hover:bg-white hover:text-blue-700 transition-all">Sell Your First Product</Link>
                 </div>
             </div>
             <img src={image} alt="Garment Production Management" className='mt-8 md:mt-0' />
